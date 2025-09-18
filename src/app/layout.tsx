ade5d0116a390362d5d935b749d626870239cbe5
Benjamin Segall <bentekkie@gmail.com>
2025-09-18 18:39:16 +0000

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: 'Daf Quizzer',
@@ -25,6 +26,7 @@ export default function RootLayout({
         {children}
         <Toaster />
       </body>
+      <GoogleAnalytics gaId="G-QKC9S8KQJQ" />
     </html>
   );
 }
